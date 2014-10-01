41cccc0c4f9597b64d22755baf51de6a1829e431
M. Liebmann <tuxcode.bbg@gmail.com>
2014-10-01 12:40:26 +0200

    ard_mediathek: Add function 'tprint()' to analyze tables.

--- plugins/ard_mediathek/ard_mediathek.lua
+++ plugins/ard_mediathek/ard_mediathek.lua
@@ -1153,6 +1153,21 @@ function conv_str(_string)
 	return _string
 end
 
+function tprint(tbl, indent)
+	if not indent then indent = 0 end
+	for k, v in pairs(tbl) do
+		formatting = string.rep(" ", indent) .. k .. ": "
+		if type(v) == "table" then
+			print(formatting)
+			tprint(v, indent+1)
+		elseif type(v) == 'boolean' then
+			print(formatting .. tostring(v))	
+		else
+			print(formatting .. v)
+		end
+	end
+end
+
 -- #######################################################################################
 --  Einstellungen
 -- #######################################################################################
