43ebed590536354e9fc1b2d2797919657f2521e4
M. Liebmann <tuxcode.bbg@gmail.com>
2015-11-25 18:37:20 +0100

    coolithek/functions.lua: Add url_encode()/url_decode() functions

--- coolithek/functions.lua
+++ coolithek/functions.lua
@@ -1,4 +1,23 @@
 
+-- url_decode/url_encode code from: http://lua-users.org/wiki/StringRecipes
+function url_decode(str)
+	str = string.gsub (str, "+", " ")
+	str = string.gsub (str, "%%(%x%x)",
+		function(h) return string.char(tonumber(h,16)) end)
+	str = string.gsub (str, "\r\n", "\n")
+	return str
+end
+
+function url_encode(str)
+	if (str) then
+		str = string.gsub (str, "\n", "\r\n")
+		str = string.gsub (str, "([^%w %-%_%.%~])",
+			function (c) return string.format ("%%%02X", string.byte(c)) end)
+		str = string.gsub (str, " ", "+")
+	end
+	return str
+end
+
 function decodeImage(b64Data, imgTyp, path)
 	local tmpImg = os.tmpname()
 	local retImg
