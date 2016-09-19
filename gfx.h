40f7f76e36bffc200eaab5dbf9c5b01fea2aaeee
svenhoefer <svenhoefer@svenhoefer.com>
2016-09-19 18:37:09 +0200

    - shellexec: enlarge "icon"

--- gfx.h
+++ gfx.h
@@ -3,6 +3,7 @@
 
 void RenderBox(int sx, int sy, int ex, int ey, int mode, int color);
 void RenderCircle(int sx, int sy, char type);
+size_t GetCircleHeight();
 //void PaintIcon(char *filename, int x, int y, unsigned char offset);
 
 #endif
