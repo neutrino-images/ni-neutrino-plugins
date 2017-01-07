8b769b20fefb1f40a51b6dfde719e4fd556ab444
Jacek Jendrzej <satbaby@kawaii.com>
2017-01-07 22:44:55 +0100

    add js_descramble

--- plugins/webtv/yt_live.lua
+++ plugins/webtv/yt_live.lua
@@ -32,34 +32,122 @@ end
 function unescape_uri(url)
   return url:gsub("%%(%x%x)", hex2char)
 end
+function js_extract(data,patern)
+	for  line  in  data:gmatch("(.-};)"  )  do
+		local m = line:match(patern)
+		if m then
+			return m
+		end
+	end
+	return nil
+end
+-- vlc youtube.lua code
+function js_descramble( sig, js )
+	local descrambler = js_extract( js, "%.set%(\"signature\",(.-)%(" )
+	if descrambler == nil then return sig end
+	local rules = js_extract( js, descrambler.."=function%([^)]*%){(.-)};" )
+	if rules == nil then return sig end
+	local helper = rules:match(";(..)%...%(" )
+	if helper == nil then return sig end
+	local transformations = js_extract( js, "[ ,]"..helper.."={(.-)};" )
+	if transformations == nil then return sig end
+
+	-- Parse the helper object to map available transformations
+	local trans = {}
+	for meth,code in string.gmatch( transformations, "(..):function%([^)]*%){([^}]*)}" ) do
+		-- a=a.reverse()
+		if string.match( code, "%.reverse%(" ) then
+			trans[meth] = "reverse"
+		-- a.splice(0,b)
+		elseif string.match( code, "%.splice%(") then
+			trans[meth] = "slice"
+		-- var c=a[0];a[0]=a[b%a.length];a[b]=c
+		elseif string.match( code, "var c=" ) then
+			trans[meth] = "swap"
+		else
+			print("Couldn't parse unknown youtube video URL signature transformation")
+		end
+	end
+
+	-- Parse descrambling rules, map them to known transformations
+	-- and apply them on the signature
+	local missing = false
+	for meth,idx in string.gmatch( rules, "..%.(..)%([^,]+,(%d+)%)" ) do
+		idx = tonumber( idx )
+		if trans[meth] == "reverse" then
+			sig = string.reverse( sig )
+		elseif trans[meth] == "slice" then
+			sig = string.sub( sig, idx + 1 )
+		elseif trans[meth] == "swap" then
+			if idx > 1 then
+				sig = string.gsub( sig, "^(.)("..string.rep( ".", idx - 1 )..")(.)(.*)$", "%3%2%1%4" )
+			elseif idx == 1 then
+				sig = string.gsub( sig, "^(.)(.)", "%2%1" )
+			end
+		else
+			print("Couldn't apply unknown youtube video URL signature transformation")
+			missing = true
+		end
+	end
+	if missing then
+		print( "Couldn't process youtube video URL, please check for updates to this script" )
+	end
+	return sig
+end
 
 function getAlternatevideourl(youtube_url,newname)
 	local id = youtube_url:match('v=(.-)$')
-	local url = 'http://www.youtube.com/get_video_info?video_id=' .. id .. '&el=embedded&ps=default&eurl=&gl=US&hl=en'
+	local url = 'https://www.youtube.com/get_video_info?video_id=' .. id .. '&el=detailpage&ps=default&eurl=&gl=US&hl=en'
 	local data = getdata(url)
 	if data then
-		local stream_map = data:gsub("^(.-)url_encoded_fmt_stream_map","")
+		local stream_map = string.match( data, "url_encoded_fmt_stream_map(.-)$" )
 		data = nil
 		if stream_map == nil then
 			return 0
 		end
 		stream_map=unescape_uri(stream_map)
+		stream_map = string.gsub( stream_map, "\\u0026", "&" )
 
 		if stream_map == nil then
 			return 0
 		end
 		if stream_map then
 			local count = 0
-			for d in stream_map:gmatch('url=(.-)[,;]') do
+			for d in stream_map:gmatch("(.-)[,;]") do
 				local item={}
 				d=unescape_uri(d)
 				local itagstr = d:match('itag=(%w+)')
 				if  itagstr ~= nil and itags[tonumber(itagstr)] then
 					local itagnum = tonumber(itagstr)
 					d=d:gsub("(&itag=%d+)","")
-					local video_url = d
+					local sig = d:match("&s=(%w+%.%w+)")
+					local sigstr = "s"
+					if not sig then
+						sig = d:match("&sig=(%w+%.%w+)")
+						sigstr = "sig"
+					end
+
+					if sig then
+						d=d:gsub("&" .. sigstr .. "=" .. sig,"")
+					end
+					local video_url = d:match("url=(.-)$")
 					if video_url ~= nil then
 						if  itags[itagnum] then
+							if sig then
+								local jsdata = getdata("https://www.youtube.com/watch?v=" .. id .. "&ps=default&eurl=&gl=US&hl=en")
+								local jsurl = jsdata:match('js":"(.-player%-en_US.-)"')
+								if jsurl then
+									jsurl = jsurl:gsub("\\","")
+									jsurl = "https:" .. jsurl
+									jsdata = getdata(jsurl)
+									if sig and jsdata then
+										sig = js_descramble(sig,jsdata)
+										video_url=video_url .. "&signature="..sig
+									end
+								else
+									print("player url not found")
+								end
+							end
 							entry = {}
 							entry['url']  = video_url .. "&itag=" .. itagnum
 							entry['band'] = "1" --dummy
