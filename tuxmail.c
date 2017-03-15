bbd136a18ede871c8e55b8030abb3404d33de806
svenhoefer <svenhoefer@svenhoefer.com>
2017-03-15 21:55:14 +0100

    - align to new neutrino.conf keywords

--- tuxmail.c
+++ tuxmail.c
@@ -6,9 +6,9 @@
 void read_neutrino_osd_conf ( int *ex,int *sx,int *ey, int *sy, int *preset)
 {
 	const char *filename="/var/tuxbox/config/neutrino.conf";
-	const char spres[][5]={"","_crt","_lcd"};
+	const char spres[][4]={"","crt","lcd"};
 	char sstr[4][32];
-	int pres=-1, loop, *sptr[5]={ex, sx, ey, sy, preset};
+	int pres=-1, resolution=-1, loop, *sptr[5]={ex, sx, ey, sy, preset};
 	char *buffer;
 	size_t len;
 	ssize_t read;
@@ -20,16 +20,17 @@ void read_neutrino_osd_conf ( int *ex,int *sx,int *ey, int *sy, int *preset)
 		len = 0;
 		while ((read = getline(&buffer, &len, fd)) != -1){
 			sscanf(buffer, "screen_preset=%d", &pres);
+			sscanf(buffer, "osd_resolution=%d", &resolution);
 			*preset = pres;
 		}
 		if(buffer)
 			free(buffer);
 		rewind(fd);
 		++pres;
-		sprintf(sstr[0], "screen_EndX%s=%%d", spres[pres]);
-		sprintf(sstr[1], "screen_StartX%s=%%d", spres[pres]);
-		sprintf(sstr[2], "screen_EndY%s=%%d", spres[pres]);
-		sprintf(sstr[3], "screen_StartY%s=%%d", spres[pres]);
+		sprintf(sstr[0], "screen_EndX_%s_%d=%%d", spres[pres], resolution);
+		sprintf(sstr[1], "screen_StartX_%s_%d=%%d", spres[pres], resolution);
+		sprintf(sstr[2], "screen_EndY_%s_%d=%%d", spres[pres], resolution);
+		sprintf(sstr[3], "screen_StartY_%s_%d=%%d", spres[pres], resolution);
 
 		buffer=NULL;
 		len = 0;
