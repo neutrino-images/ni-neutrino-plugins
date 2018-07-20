68bc0db0054086a5da5f4a3912c1c7394e25639f
Markus Volk <f_l_k@t-online.de>
2018-07-21 00:27:59 +0200

    addition to last commit

--- plugins/stb_backup/stb-backup.lua
+++ plugins/stb_backup/stb-backup.lua
@@ -24,9 +24,9 @@
 -- authors and should not be interpreted as representing official policies, either expressed
 -- or implied, of the Tuxbox Project.
 
-The views and conclusions contained in the software and documentation are those of the
-authors and should not be interpreted as representing official policies, either expressed
-or implied, of the Tuxbox Project.
+-- The views and conclusions contained in the software and documentation are those of the
+-- authors and should not be interpreted as representing official policies, either expressed
+-- or implied, of the Tuxbox Project.
 
 caption = "STB-Backup"
 
