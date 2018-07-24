90492e2e0d9d0991e01d99303b427f40b45b3f0d
Markus Volk <f_l_k@t-online.de>
2018-07-24 13:13:33 +0200

    stb-startup-plugin: add bsd license to the file

--- plugins/stb_startup/stb-startup.lua
+++ plugins/stb_startup/stb-startup.lua
@@ -1,9 +1,30 @@
--- Plugin to change the startup partition
--- (c) Markus Volk, Sven Hoefer, Don de Deckelwech
--- distributed under BSD-2-Clause License
+-- The Tuxbox Copyright
+--
+-- Copyright 2018 Markus Volk, Sven Hoefer, Don de Deckelwech
+--
+-- Redistribution and use in source and binary forms, with or without modification, 
+-- are permitted provided that the following conditions are met:
+--
+-- Redistributions of source code must retain the above copyright notice, this list
+-- of conditions and the following disclaimer. Redistributions in binary form must
+-- reproduce the above copyright notice, this list of conditions and the following
+-- disclaimer in the documentation and/or other materials provided with the distribution.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS`` AND ANY EXPRESS OR IMPLIED
+-- WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+-- AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+-- EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-- The views and conclusions contained in the software and documentation are those of the
+-- authors and should not be interpreted as representing official policies, either expressed
+-- or implied, of the Tuxbox Project.
 
 caption = "STB-StartUp"
-version = 0.04
 
 n = neutrino()
 fh = filehelpers.new()
@@ -42,7 +63,7 @@ chooser = cwindow.new {
 	y = chooser_y,
 	dx = chooser_dx,
 	dy = chooser_dy,
-	title = caption .. " v" .. version,
+	title = caption,
 	icon = "settings",
 	has_shadow = true,
 	btnRed = "Partition 1",
@@ -92,7 +113,7 @@ chooser:hide()
 
 if colorkey then
 	res = messagebox.exec {
-		title = caption .. " v" .. version,
+		title = caption,
 		icon = "settings",
 		text = locale[lang].start_partition,
 		timeout = 0,
