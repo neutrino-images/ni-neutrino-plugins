f7b4500c4656bbac7c769f1794854ae41989c234
max_10 <max_10@gmx.de>
2019-03-29 09:45:12 +0100

    - fix plugin datadir

--- input/icons.h
+++ input/icons.h
@@ -1,13 +1,12 @@
 #ifndef __icons_h__
 #define __icons_h__
 
+#define ICON_BUTTON_RED     DATADIR "/neutrino/icons/rot.png"
+#define ICON_BUTTON_GREEN   DATADIR "/neutrino/icons/gruen.png"
+#define ICON_BUTTON_YELLOW  DATADIR "/neutrino/icons/gelb.png"
+#define ICON_BUTTON_BLUE    DATADIR "/neutrino/icons/blau.png"
 
-#define ICON_BUTTON_RED     "/share/tuxbox/neutrino/icons/rot.png"
-#define ICON_BUTTON_GREEN   "/share/tuxbox/neutrino/icons/gruen.png"
-#define ICON_BUTTON_YELLOW  "/share/tuxbox/neutrino/icons/gelb.png"
-#define ICON_BUTTON_BLUE    "/share/tuxbox/neutrino/icons/blau.png"
-
-#define ICON_KEYS           "/share/tuxbox/neutrino/icons/hint_keys.png"
-#define ICON_NUMERIC_PAD    "/share/tuxbox/neutrino/icons/numericpad.png"
+#define ICON_KEYS           DATADIR "/neutrino/icons/hint_keys.png"
+#define ICON_NUMERIC_PAD    DATADIR "/neutrino/icons/numericpad.png"
 
 #endif
--- shellexec/icons.h
+++ shellexec/icons.h
@@ -2,9 +2,9 @@
 #define __icons_h__
 
 
-#define ICON_BUTTON_RED     "/share/tuxbox/neutrino/icons/rot.png"
-#define ICON_BUTTON_GREEN   "/share/tuxbox/neutrino/icons/gruen.png"
-#define ICON_BUTTON_YELLOW  "/share/tuxbox/neutrino/icons/gelb.png"
-#define ICON_BUTTON_BLUE    "/share/tuxbox/neutrino/icons/blau.png"
+#define ICON_BUTTON_RED     DATADIR "/neutrino/icons/rot.png"
+#define ICON_BUTTON_GREEN   DATADIR "/neutrino/icons/gruen.png"
+#define ICON_BUTTON_YELLOW  DATADIR "/neutrino/icons/gelb.png"
+#define ICON_BUTTON_BLUE    DATADIR "/neutrino/icons/blau.png"
 
 #endif
