6df3970d10737a1f6bef3a107e089e1929037cc4
vanhofen <vanhofen@gmx.de>
2019-05-24 05:25:28 +0200

    - imgbackup-hd51: support for new partition layout; add VU+ stb's
    thanks to DBoxOldie and BPanther

--- scripts-sh/plugins/imgbackup-hd51/imgbackup-hd51.sh
+++ scripts-sh/plugins/imgbackup-hd51/imgbackup-hd51.sh
@@ -1,54 +1,97 @@
 #!/bin/sh
 # Script for complete image backup
-# (C) 2018 DboxOldie
+# (C) 2018-2019 DboxOldie / BPanther
 # License: GPLv2 or later
-Version="1.0 v. 13.09.2018"
+Version="1.03 vom 23.05.2019"
 #
 file=$0
 model=`cat /proc/stb/info/model`
+[ -e /proc/stb/info/vumodel ] && vumodel=`cat /proc/stb/info/vumodel`
+[ "$model" == "dm8000" ] && [ "$vumodel" == "solo4k" ] && model=$vumodel
 save_path="/tmp/imgbackup"
 tmproot="/tmp/buroot"
-destname="imgbackup-hd51_$(date +%d.%m.%Y-%H.%M)"
+destname="imgbackup-${model}_$(date +%d.%m.%Y-%H.%M)"
 archive="tgz"
 knl=0
 klen=0
 bz2=$(which bzip2)
 hexd=$(which hexdump)
+new_layout=0
 #
-magic_number="0x016f2818"
-#magic_number="0x016f2810"
+magic_number="0x016f2818" # HD51 / VUSOLO4K
 dtb_magic_number="0xd00dfeed"
+#
+# Subroutine für hd51
+read_bootargs()
+{
+rootsubdir=""
+kdev=""
+for i in $(cat /sys/firmware/devicetree/base/chosen/bootargs);do
+	[ -n "$(echo $i | grep 'root=')" ] && rootmtd=$(echo $i | cut -d '=' -f2 | cut -d '/' -f3)
+	[ -n "$(echo $i | grep 'rootsubdir=')" ] && rootsubdir=$(echo $i | cut -d '=' -f2)
+	[ -n "$(echo $i | grep 'kernel=')" ] && kdev=$(echo $i | cut -d '=' -f2)
+done
+[ -n "$rootsubdir" -a -n "$kdev" ] && new_layout=1
+[ $new_layout == 1 ] && kernelnumber=$(echo ${kdev:13:1})
+}
 #
 # Root und Kernel Partition bestimmen
 #
-rootmtd=`readlink /dev/root`
-rootnumber=`echo ${rootmtd:8:1}`
-mmcprefix=`echo ${rootmtd:0:8}`
-kernelnumber=$((rootnumber -1))
-kernelmtd=$mmcprefix$kernelnumber
-
-echo "  HD51 Image Backup (Version: $Version)"
 if [ "$model" == "hd51" ];then
-	echo "  Image Backup für Boxmodel '$model' startet ..."
+	read_bootargs
 else
-	echo "  Falsche Box. Abbruch!"
+	rootmtd=`readlink /dev/root`
+fi
+rootnumber=`echo ${rootmtd:8:2}`
+mmcprefix=`echo ${rootmtd:0:8}`
+
+echo "  AX HD51 4K und VU+ SOLO 4K Image Backup (Version: $Version)"
+if [ "$model" == "hd51" ];then
+	echo "  Image Backup für Boxmodel '$model' startet..."
+	[ $new_layout == 0 ] && kernelnumber=$((rootnumber - 1))
+elif [ "$model" == "solo4k" ];then
+	case $rootmtd in
+		mmcblk0p4)
+			kernelnumber=$((rootnumber - 3));;
+		mmcblk0p5)
+			multipart=1
+			kernelnumber=$((rootnumber - 1));;
+		mmcblk0p7)
+			multipart=2
+			kernelnumber=$((rootnumber - 1));;
+		mmcblk0p9)
+			multipart=3
+			kernelnumber=$((rootnumber - 1));;
+		mmcblk0p11)
+			multipart=4
+			kernelnumber=$((rootnumber - 1));;
+	esac
+	echo "  Image Backup für Boxmodel '$model' startet..."
+else
+	echo "  Falsche Box...Abbruch"
 	exit 0
 fi
+kernelmtd=$mmcprefix$kernelnumber
 
 if [ -z "$bz2" ];then
 	echo "  Kein 'bzip2' im Image"
-	echo "  Packen von 'rootfs.tar.bz2' nicht möglich"
-	echo "  Abbruch!"
+	echo "  packen von 'rootfs${multipart}.tar.bz2' nicht möglich"
+	echo "  Abbruch... !!"
 	exit 0
 fi
 
-if [ $kernelnumber -lt 9 -a $kernelnumber -gt 1 ];then
+if [ $kernelnumber -lt 12 -a $kernelnumber -gt 0 ];then
 	echo
-	echo "  Bootdevice   = $rootmtd"
+	if [ $new_layout == 1 ];then
+		echo "  -$model neues Flashlayout-"
+		echo "  Bootdevice   = $rootsubdir auf $rootmtd"
+	else
+		echo "  Bootdevice   = $rootmtd"
+	fi
 	echo "  Kerneldevice = $kernelmtd"
 else
-	echo "  Kernel MTD nicht im Bereich (2,4,6,8)! > $kernelnumber"
-	echo "  Abbruch!"
+	echo "  Kernel MTD nicht im Bereich ( 1..11 ) !! > $kernelnumber"
+	echo "  Abbruch !!"
 	exit 0
 fi
 
@@ -132,7 +175,7 @@ if [ "$knl" == "1" ];then
 		dtb_len=$dtb_len`echo $line | cut -d " " -f9`
 		echo "  DTB Länge = $((dtb_len)) Bytes"
 	else
-		echo "$gelb Kein DTB Bereich vorhanden"
+		echo "$gelb  Kein DTB Bereich vorhanden"
 		dtb_len=0
 	fi
 
@@ -156,12 +199,13 @@ rest=`echo $((klen - len1))`
 dd if=/dev/$kernelmtd of=$save_path/kernel_1.bin bs=4096 count=$count 2> /dev/null
 dd if=/dev/$kernelmtd of=$save_path/kernel_2.bin bs=1 count=$rest skip=$len1 2> /dev/null
 
-cat $save_path/kernel_*.bin > $save_path/kernel.bin
+[ "$model" == "solo4k" ] && extname=${multipart}_auto
+cat $save_path/kernel_?.bin > $save_path/kernel$extname.bin
 
-rm -f $save_path/kernel_*.bin
+rm -f $save_path/kernel_?.bin
 
 echo
-echo "  'kernel.bin' in $save_path gespeichert"
+echo "  'kernel$extname.bin' in $save_path gespeichert"
 }
 #
 r_backup()
@@ -170,25 +214,25 @@ mkdir -p $tmproot
 mount --bind / $tmproot
 
 echo
-echo "  erstelle 'rootfs.tar'"
-tar -cf $save_path/rootfs.tar -C $tmproot ./ 2> /dev/null
-echo "  packe 'rootfs.tar' zu 'rootfs.tar.bz2'"
-echo "  dauert ca. 1 bis 2 Minuten ..."
-$bz2 $save_path/rootfs.tar
+echo "  erstelle 'rootfs${multipart}.tar'"
+tar -cf $save_path/rootfs${multipart}.tar -C $tmproot ./ 2> /dev/null
+echo "  packe 'rootfs${multipart}.tar' zu rootfs${multipart}.tar.bz2'"
+echo "  dauert ca. 2 Minuten...."
+$bz2 $save_path/rootfs${multipart}.tar
 
 umount -f $tmproot
-[ -z "$(mount | grep $tmproot)" ] && rm -rf $tmproot
+[ -z "$(mount | grep $tmproot)" ] && rmdir $tmproot
 
 if [ "$archive" == "tgz" ];then
 	echo "  erstelle Image Archiv '$destname.tgz' in $save_path"
-	tar -czf $save_path/$destname.tgz -C $save_path kernel.bin rootfs.tar.bz2
-	rm -f $save_path/kernel.bin
-	rm -f $save_path/rootfs.tar.bz2
+	tar -czf $save_path/$destname.tgz -C $save_path kernel$extname.bin rootfs${multipart}.tar.bz2
+	rm -f $save_path/kernel$extname.bin
+	rm -f $save_path/rootfs${multipart}.tar.bz2
 	echo
 	echo "  Image Archiv '$destname.tgz' gespeichert in $save_path"
 else
 	echo
-	echo "  'rootfs.tar.bz2' in $save_path gespeichert"
+	echo "  'rootfs${multipart}.tar.bz2' in $save_path gespeichert"
 fi
 
 }
