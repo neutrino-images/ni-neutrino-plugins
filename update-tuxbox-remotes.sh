ead26614385e043612e5f62a28924507dbc22470
vanhofen <vanhofen@gmx.de>
2019-05-26 23:50:34 +0200

    - export GIT_MERGE_AUTOEDIT=no before syncing with remote repos

--- update-tuxbox-remotes.sh
+++ update-tuxbox-remotes.sh
@@ -2,6 +2,7 @@
 
 REPLIST="cooliTSclimax getrc input logomask logoview msgbox scripts-lua shellexec tuxcal tuxcom tuxmail tuxwetter"
 
+export GIT_MERGE_AUTOEDIT=no
 for plugin in $REPLIST; do
 	git subtree pull --prefix=$plugin https://github.com/tuxbox-neutrino/plugin-$plugin.git master || exit 1
 done
