bc34bcb95612398c59b730de850d193d6db90ac5
Markus Volk <f_l_k@t-online.de>
2019-06-05 03:22:38 +0200

    stb-startup.lua: cleanup

--- plugins/stb_startup/stb-startup.lua
+++ plugins/stb_startup/stb-startup.lua
@@ -27,18 +27,15 @@
 on = "ein"; off = "aus"
 
 function exists(file)
-	local ok, err, exitcode = os.rename(file, file)
-	if not ok then
-		if exitcode == 13 then
-			-- Permission denied, but it exists
-			return true
-		end
-	end
-	return ok, err
+	return fh:exist(file, "f")
 end
 
 function isdir(path)
-	return exists(path .. "/")
+	return fh:exist(path, "d")
+end
+
+function islink(path)
+	return fh:exist(path, "l")
 end
 
 function mkdir(path)
@@ -81,7 +78,8 @@ end
 
 function mount_filesystems()
 	for _,v in ipairs(partlabels) do
-		if exists(partitions_by_name .. "/" .. v) then
+		if exists(partitions_by_name .. "/" .. v) or
+		islink(partitions_by_name .. "/" .. v) then
 			mkdir("/tmp/testmount/" .. v)
 			mount(partitions_by_name .. "/" .. v,"/tmp/testmount/" .. v)
 		end
@@ -100,7 +98,8 @@ end
 
 function umount_filesystems()
 	for _,v in ipairs(partlabels) do
-		if exists(partitions_by_name .. "/" .. v) then
+		if exists(partitions_by_name .. "/" .. v) or
+		islink(partitions_by_name .. "/" .. v) then
 			umount("/tmp/testmount/" .. v)
 		end
 		if is_mounted("/tmp/testmount/" .. v) then
@@ -119,7 +118,7 @@ function reboot()
 	umount_filesystems()
 	if exists("/bin/systemctl") then
 		local file = assert(io.popen("systemctl reboot"))
-	elseif fh:exist("/sbin/init", "f") then
+	elseif exists("/sbin/init") then
 		local file = assert(io.popen("sync && init 6"))
 	else
 		local file = assert(io.popen("reboot"))
@@ -131,16 +130,16 @@ function basename(str)
 	return name
 end
 
-function get_value(str,part,etcdir)
+function get_value(str,root,etcdir)
 	if is_mounted("/tmp/testmount/userdata") then
-		for line in io.lines("/tmp/testmount/linuxrootfs" .. part  .. etcdir .. "/image-version") do
+		for line in io.lines("/tmp/testmount/linuxrootfs" .. root  .. etcdir .. "/image-version") do
 			if line:match(str .. "=") then
 				local i,j = string.find(line, str .. "=")
 				value = string.sub(line, j+1, #line)
 			end
 		end
-	elseif is_mounted("/tmp/testmount/rootfs" .. part) then
-		for line in io.lines("/tmp/testmount/rootfs" .. part  .. etcdir .. "/image-version") do
+	elseif is_mounted("/tmp/testmount/rootfs" .. root) then
+		for line in io.lines("/tmp/testmount/rootfs" .. root  .. etcdir .. "/image-version") do
 			if line:match(str .. "=") then
 				local i,j = string.find(line, str .. "=")
 				value = string.sub(line, j+1, #line)
@@ -151,14 +150,8 @@ function get_value(str,part,etcdir)
 end
 
 function get_imagename(root)
-	local etc_isdir = false
-	if fh:exist("/tmp/testmount/linuxrootfs" .. root .. "/etc", "d") or
-	fh:exist("/tmp/testmount/rootfs" .. root .. "/etc", "d") then
-		etc_isdir = true
-	end
-	if etc_isdir and
-	(exists("/tmp/testmount/linuxrootfs" .. root .. "/etc/image-version") or
-	exists("/tmp/testmount/rootfs" .. root  .. "/etc/image-version")) then
+	if exists("/tmp/testmount/linuxrootfs" .. root .. "/etc/image-version") or
+	exists("/tmp/testmount/rootfs" .. root  .. "/etc/image-version") then
 		imagename = get_value("distro", root, "/etc") .. " " .. get_value("imageversion", root, "/etc")
 	elseif exists("/tmp/testmount/linuxrootfs" .. root .. "/var/etc/image-version") or
 	exists("/tmp/testmount/rootfs" .. root  .. "/var/etc/image-version") then
@@ -404,9 +397,9 @@ function main()
 	chooser:hide()
 
 	if colorkey then
-		if exists("/tmp/testmount/" .. devbase .. root .. "/usr") then
+		if islink("/tmp/testmount/" .. devbase .. root) then
 			-- found image folder
-		elseif isdir("/tmp/testmount/rootfs" .. root .. "/usr") then
+		elseif isdir("/tmp/testmount/rootfs" .. root) then
 			-- found image folder
 		else
 			local ret = hintbox.new { title = caption, icon = "settings", text = locale[lang].empty_partition };
