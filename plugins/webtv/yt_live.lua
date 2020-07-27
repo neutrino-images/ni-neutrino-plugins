cebe46ee2d3f733839e3c2ed72a57e9ab62410f8
Jacek Jendrzej <overx300@gmail.com>
2020-07-27 14:29:02 +0200

    webtv: fix YT

--- plugins/webtv/yt_live.lua
+++ plugins/webtv/yt_live.lua
@@ -146,7 +146,7 @@ function getVideoData(yurl)
 	if yurl:find("www.youtube.com/user/") or yurl:find("youtube.com/channel") or yurl:find("youtube.com/c/") then --check user link or channel alias
 		local youtube_user = getdata(yurl)
 		if youtube_user == nil then return 0 end
-		local youtube_live_url = youtube_user:match('feature=c4.-href="(/watch.-)"')
+		local youtube_live_url = youtube_user:match('"url":"(/watch.-)"') or youtube_user:match('feature=c4.-href="(/watch.-)"')
 		if youtube_live_url == nil then return 0 end
 		yurl = 'https://www.youtube.com' .. youtube_live_url
 	end
@@ -219,37 +219,47 @@ function getVideoData(yurl)
 				url_map = data:match('url_encoded_fmt_stream_map=(.-)$' )
 				if url_map then url_map=unescape_uri(url_map) end
 			end
-			local map_urls = {}
 			local player_map = data:match("ytplayer.config%s+=%s+({.-});")
+			local map_urls = {}
 			local ucount = 0
 			if player_map then
-				local pm1 = nil
-				local pm2 = nil
-				local pm1 = json:decode (player_map)
-				if pm1 and pm1.args and pm1.args.player_response then
-					pm2 = json:decode(pm1.args.player_response)
-					if pm2 and pm2.streamingData and pm2.streamingData.formats then
-						for k, purl in pairs(pm2.streamingData.formats) do
-							if purl.itag and have_itags[purl.itag] ~= true then
-								have_itags[purl.itag] = true
+				local formats_data = data:match('\\"formats\\":(%[{.-}])')
+				if formats_data then
+					formats_data = formats_data:gsub('\\\\\\"','')
+					formats_data = formats_data:gsub('\\"','"')
+					local formats = json:decode (formats_data)
+					if formats then
+						for k, purl in pairs(formats) do
+							if itag and have_itags[itag] ~= true then
+								have_itags[itag] = true
 								ucount = ucount + 1
-								if purl.cipher then
-									map_urls[ucount] = purl.cipher
-								elseif purl.url then
-									map_urls[ucount] = "url=" .. purl.url
+								if signatureCipher then
+									map_urls[ucount] = signatureCipher
+								elseif url then
+									map_urls[ucount] = "url=" .. url
+								elseif cipher then --unnecessary?
+									map_urls[ucount] = cipher
 								end
 							end
 						end
-					end
-					if pm2 and pm2.streamingData and pm2.streamingData.adaptiveFormats then
-						for k, purl in pairs(pm2.streamingData.adaptiveFormats) do
-							if purl.itag and have_itags[purl.itag] ~= true then
-								have_itags[purl.itag] = true
-								ucount = ucount + 1
-								if purl.cipher then
-									map_urls[ucount] = purl.cipher
-								elseif purl.url then
-									map_urls[ucount] = "url=" .. purl.url
+						local adaptiveFormats_data = data:match('\\"adaptiveFormats\\":(%[{.-}])')
+						if adaptiveFormats_data then
+							adaptiveFormats_data = adaptiveFormats_data:gsub('\\\\\\"','')
+							adaptiveFormats_data = adaptiveFormats_data:gsub('\\"','"')
+							local adaptiveFormats = json:decode (adaptiveFormats_data)
+							if adaptiveFormats then
+								for k, purl in pairs(adaptiveFormats) do
+									if purl.itag and have_itags[purl.itag] ~= true then
+										have_itags[purl.itag] = true
+										ucount = ucount + 1
+										if purl.signatureCipher then
+											map_urls[ucount] = purl.signatureCipher
+										elseif purl.url then
+											map_urls[ucount] = "url=" .. purl.url
+										elseif purl.cipher then --unnecessary?
+											map_urls[ucount] = purl.cipher
+										end
+									end
 								end
 							end
 						end
@@ -298,7 +308,7 @@ function getVideoData(yurl)
 					local s=myurl:match('6s=([%%%-%=%w+_]+)') or myurl:match('&s=([%%%-%=%w+_]+)') or myurl:match('^s=([%%%-%=%w+_]+)')
 					if s and (#s > 99 and #s < 160) then
 						local s2=unescape_uri(s)
-						local js_url= data:match('<script src="([/%w%p]+base%.js)"')
+						local js_url= data:match('<script%s+src="([/%w%p]+base%.js)"')
 						local signature = newsig(s2,js_url)
 						if signature then
 							s = s:gsub("[%+%?%-%*%(%)%.%[%]%^%$%%]","%%%1")
