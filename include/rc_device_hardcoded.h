4766da0a7231996e867d5e7d6cd2c111fedaff86
Markus Volk <f_l_k@t-online.de>
2020-08-03 20:38:44 +0200

    rc_device_hardcoded.h: add osmio4k and osmio4kplus

--- include/rc_device_hardcoded.h
+++ include/rc_device_hardcoded.h
@@ -16,6 +16,22 @@
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
 #endif
 
+#elif BOXMODEL_OSMIO4K
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event0"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#endif
+
+#elif BOXMODEL_OSMIO4KPLUS
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event0"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#endif
+
 #else
 #ifndef RC_DEVICE
 #define RC_DEVICE "/dev/input/event1"
