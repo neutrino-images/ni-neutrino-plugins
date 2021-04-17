6b16909bb3926c09b34b2558af87f6a58d289bd5
BPanther <bpanther_ts@hotmail.com>
2021-04-17 11:53:51 +0200

    - imgbackup: update to version 1.30

--- scripts-sh/plugins/imgbackup/imgbackup.sh
+++ scripts-sh/plugins/imgbackup/imgbackup.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
-model=`cat /proc/stb/info/model`
-[ -e /proc/stb/info/vumodel ] && vumodel=`cat /proc/stb/info/vumodel`
-[ "$model" == "dm8000" ] && [ "$vumodel" == "solo4k" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "duo4k" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "duo4kse" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "zero4k" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "ultimo4k" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "uno4k" ] && model=$vumodel
-[ "$model" == "dm8000" ] && [ "$vumodel" == "uno4kse" ] && model=$vumodel
-rootmtd=`readlink /dev/root`
-if [ "$model" == "solo4k" -o "$model" == "ultimo4k" -o "$model" == "uno4k" -o "$model" == "uno4kse" ] && [ "$rootmtd" == "mmcblk0p4" ] || \
-   [ "$model" == "zero4k" ] && [ "$rootmtd" == "mmcblk0p7" ] || \
-   [ "$model" == "duo4k" -o "$model" == "duo4kse" ] && [ "$rootmtd" == "mmcblk0p9" ]; then
-	/usr/share/tuxbox/neutrino/plugins/imgbackup select_dir
-else
-	/usr/share/tuxbox/neutrino/plugins/imgbackup gui
-fi
+/usr/share/tuxbox/neutrino/plugins/imgbackup gui
 exit 0
