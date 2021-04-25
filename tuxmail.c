350842cade535f04a85730461f9b57413acda90c
GetAway <get-away@t-online.de>
2021-04-25 21:11:33 +0200

    fix reading neutrino's screen presets

--- tuxmail.c
+++ tuxmail.c
@@ -7,9 +7,9 @@
 void read_neutrino_osd_conf ( int *ex,int *sx,int *ey, int *sy, int *preset)
 {
 	const char *filename = CONFIGDIR "/neutrino.conf";
-	const char spres[][4]={"","crt","lcd"};
+	const char spres[][4]={"", "crt", "lcd", "a", "b"};
 	char sstr[4][32];
-	int pres=-1, resolution=-1, loop, *sptr[5]={ex, sx, ey, sy, preset};
+	int step=0, pres=-1, resolution=-1, loop, *sptr[5]={ex, sx, ey, sy, preset};
 	char *buffer;
 	size_t len;
 	ssize_t read;
@@ -20,6 +20,8 @@ void read_neutrino_osd_conf ( int *ex,int *sx,int *ey, int *sy, int *preset)
 		buffer=NULL;
 		len = 0;
 		while ((read = getline(&buffer, &len, fd)) != -1){
+			if(strstr(buffer, "screen_EndX_a"))
+				step = 2;
 			sscanf(buffer, "screen_preset=%d", &pres);
 			sscanf(buffer, "osd_resolution=%d", &resolution);
 			*preset = pres;
@@ -28,6 +30,7 @@ void read_neutrino_osd_conf ( int *ex,int *sx,int *ey, int *sy, int *preset)
 			free(buffer);
 		rewind(fd);
 		++pres;
+		pres += step;
 		if (resolution == -1)
 		{
 			sprintf(sstr[0], "screen_EndX_%s=%%d", spres[pres]);
@@ -3637,7 +3640,7 @@ void SaveAndReloadDB ( int iSave )
 //void plugin_exec(PluginParam *par)
 int main ( void )
 {
-	char cvs_revision[] = "$Revision: 1.03 $";
+	char cvs_revision[] = "$Revision: 1.04 $";
 	int loop, account, mailindex;
 	FILE *fd_run;
 	FT_Error error;
