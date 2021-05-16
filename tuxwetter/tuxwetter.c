5c90fd3236f8c7ff3b16ac3eb3716438c883071f
max_10 <max_10@gmx.de>
2021-05-17 00:19:55 +0200

    - tuxwetter: fix GENERIC build

--- tuxwetter/tuxwetter.c
+++ tuxwetter/tuxwetter.c
@@ -166,6 +166,7 @@ int rclocked=0;
 int swidth;
 int stride;
 
+#if !HAVE_GENERIC_HARDWARE
 static inline int dev_open()
 {
 	int fd = open(DISPLAY_DEV, DISPLAY_OPMODE);
@@ -190,6 +191,7 @@ void ShowText(const char * str)
 #endif
 	close(fd);
 }
+#endif
 
 int get_instance(void)
 {
@@ -870,7 +872,9 @@ char *lcptr = NULL, *lcstr= NULL, *lcdptr = NULL;
 			}
 			*(lcdptr++)=*(lcptr++);
 		}
+#if !HAVE_GENERIC_HARDWARE
 		ShowText(lcstr);
+#endif
 #if 0
 		*lcptr=0;
 		LCD_Clear();
