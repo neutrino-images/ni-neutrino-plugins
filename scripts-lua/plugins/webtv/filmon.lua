f446280bf7fbd9a26fedf964e50bcedfdea1ab25
vanhofen <vanhofen@gmx.de>
2021-09-26 23:04:12 +0200

    - filmon: update lua script

--- scripts-lua/plugins/webtv/filmon.lua
+++ scripts-lua/plugins/webtv/filmon.lua
@@ -1,5 +1,5 @@
-local n = neutrino(0, 0, SCREEN.X_RES, SCREEN.Y_RES);
-n:checkVersion(1, 31);
+M = misc.new()
+M:checkVersion(1, 31)
 
 json = require "json"
 
@@ -22,48 +22,64 @@ function getdata(Url)
 end 
 
 function getVideoData(url)
-	if url == nil then return 0 end
-	local data = getdata('http://www.filmon.com/tv/channel/info/' .. url)
-	if data == nil then return 0 end
+	if url == nil then
+		return 0
+	end
+	local data = getdata("http://www.filmon.com/tv/channel/info/" .. url)
+	if data == nil then
+		return 0
+	end
 
-	local data = json:decode(data) 
-	if data==nil then return 0 end
-	data = data['data']
-	if data==nil then return 0 end
+	local data = json:decode(data)
+	if data == nil then
+		return 0
+	end
+	data = data["data"]
+	if data == nil then
+		return 0
+	end
 
-    local title=data['title']
-    if title==nil then name='nil' end
-    title=title .. ' - filmon'
+	local title = data["title"]
+	if title == nil then
+		name = "nil"
+	end
+	title = title .. " - FilmOn"
 
-	data = data['streams']
-	if data==nil then return 0 end
-	
-	local highurl=nil
-	local lowurl=nil
-  local quality=nil
-  local surl=nil
-    for i,stream in ipairs(data) do
-      quality=stream['quality']
-      surl=stream['url']
-      if (quality=='high')or(quality=='HD') then
-        if (stream['watch-timeout']>=3600)and(surl:find('mustbeasubscriber')==nil) then 
-    	    highurl = surl
-        end
- 	    elseif (quality=='low')or(quality=='SD') then
-	      lowurl = surl
-	   end
-    end
-    local count=0
-    if highurl ~= nil then
-     --if  lowurl ~= nil then highurl=lowurl:gsub('low.stream','high.stream') end   
-       count = 1
-       ret[1]={ url = highurl, band = "1500000", name = title, res1 = "854", res2 = "480" }
-    end
-    if lowurl ~= nil then
-       count = count+1
-       ret[count]={ url = lowurl, band = "500000", name = title, res1 = "576", res2 = "322" }
-    end
-    return count
+	data = data["streams"]
+	if data == nil then
+		return 0
+	end
+
+	local highurl = nil
+	local lowurl = nil
+	local quality = nil
+	local surl = nil
+	for i, stream in ipairs(data) do
+		quality = stream["quality"]
+		surl = stream["url"]
+		if (quality == "high") or (quality == "HD") then
+			if (stream["watch-timeout"] >= 3600) and (surl:find("mustbeasubscriber") == nil) then
+				highurl = surl
+			end
+		elseif (quality == "low") or (quality == "SD") then
+			lowurl = surl
+		end
+	end
+	local count = 0
+	if highurl ~= nil then
+--[[
+		if lowurl ~= nil then
+			highurl=lowurl:gsub('low.stream','high.stream')
+		end
+]]
+		count = 1
+		ret[1] = {url = highurl, band = "1500000", name = title, res1 = "854", res2 = "480"}
+	end
+	if lowurl ~= nil then
+		count = count + 1
+		ret[count] = {url = lowurl, band = "500000", name = title, res1 = "576", res2 = "322"}
+	end
+	return count
 end
 
 if (getVideoData(_url) > 0) then
