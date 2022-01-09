8547178aee4873ad37d22de126bfce406dd56b44
GetAway <get-away@t-online.de>
2022-01-09 13:47:51 +0100

    - fix button icons
    - use ICONSDIR define

--- icons.h
+++ icons.h
@@ -1,10 +1,13 @@
 #ifndef __icons_h__
 #define __icons_h__
 
+#ifndef ICONSDIR
+#define ICONSDIR	"/usr/share/tuxbox/neutrino/icons"
+#endif
 
-#define ICON_BUTTON_RED     "/share/tuxbox/neutrino/icons/rot.png"
-#define ICON_BUTTON_GREEN   "/share/tuxbox/neutrino/icons/gruen.png"
-#define ICON_BUTTON_YELLOW  "/share/tuxbox/neutrino/icons/gelb.png"
-#define ICON_BUTTON_BLUE    "/share/tuxbox/neutrino/icons/blau.png"
+#define ICON_BUTTON_RED     ICONSDIR "/btn_red.png"
+#define ICON_BUTTON_GREEN   ICONSDIR "/btn_green.png"
+#define ICON_BUTTON_YELLOW  ICONSDIR "/btn_yellow.png"
+#define ICON_BUTTON_BLUE    ICONSDIR "/btn_blue.png"
 
 #endif
