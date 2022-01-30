2fbd6817c777fa5c51adc6ca4cf6e54ef1eb81a7
vanhofen <vanhofen@gmx.de>
2022-01-30 22:10:45 +0100

    - tuxcom: use monospace font

--- tuxcom/tuxcom.h
+++ tuxcom/tuxcom.h
@@ -199,7 +199,7 @@ int rcaltgrtable[] =
 
 
 //freetype stuff
-#define FONT FONTDIR "/neutrino.ttf"
+#define FONT FONTDIR "/tuxtxt.ttf"
 // if font is not in usual place, we look here:
 #define FONT2 FONTDIR "/pakenham.ttf"
 
