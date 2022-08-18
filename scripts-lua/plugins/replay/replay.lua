c3bb0994185cb9947e339b398346ff9f6cb98b0e
vanhofen <vanhofen@gmx.de>
2022-08-18 21:44:19 +0200

    - replay: some reworks and fixes ...
    * drop init() function
    * drop main() function
    * some renamings
    * whitespace fixes
    * fix PlayFile() parameters

--- scripts-lua/plugins/replay/replay.lua
+++ scripts-lua/plugins/replay/replay.lua
@@ -1,29 +1,55 @@
+--[[
+	Replay Plugin
+
+	Copyright (C) 2022 Jacek Jendrzej 'satbaby'
+	Copyright (C) 2022 Sven Hoefer 'vanhofen'
+
+	License: WTFPLv2
+]]
+
 -- version 0.1 add ard
 
+plugin_name = "Replay"
+
 -- list duplicates
-local duplicates = false
---local duplicates = true
+duplicates = false
+--duplicates = true
 
 -- list specific channels only
---local channels = {1, 14, 20, 12, 5, 15, 19, 7, 36, 3, 4, 33}
-local channels = {}
+--channels = {1, 14, 20, 12, 5, 15, 19, 7, 36, 3, 4, 33}
+channels = {}
 
 if next(channels) ~= nil then
 	duplicates = true
 end
 
-function init()
-	json = require "json"
-	replayList = {}
-	replayMenu = nil
-	getReLiveList_ARD()
-	n = neutrino()
-	vPlay = video.new()
-	nMisc = misc.new()
-	repaint = false
-	Epg = nil
-	Title = nil
+json = require "json"
+n = neutrino()
+replayList = {}
+replayMenu = nil
+vPlay = video.new()
+repaint = false
+Title = nil
+Epg = nil
+
+locale = {}
+locale["deutsch"] = {
+	wait = "Bitte warten ...",
+	read_data = "Lese Daten ..."
+}
+locale["english"] = {
+	wait = "Please wait ...",
+	read_data = "Read data ..."
+}
+
+neutrino_conf = configfile.new()
+neutrino_conf:loadConfig("/var/tuxbox/config/neutrino.conf")
+lang = neutrino_conf:getString("language", "english")
+if locale[lang] == nil then
+	lang = "english"
 end
+
+-- ----------------------------------------------------------------------------
 
 function getdata(Url,Postfields,outputfile,pass_headers,httpheaders)
 	if Url == nil then return nil end
@@ -32,7 +58,7 @@ function getdata(Url,Postfields,outputfile,pass_headers,httpheaders)
 	end
 
 	if Url:sub(1, 2) == '//' then
-		Url =  'https:' .. Url
+		Url = 'https:' .. Url
 	end
 
 	local ret, data = Curl:download{ url=Url, A="Mozilla/5.0",maxRedirs=5,followRedir=false,postfields=Postfields,header=pass_headers,o=outputfile,httpheader=httpheaders }
@@ -47,8 +73,8 @@ function getdata(Url,Postfields,outputfile,pass_headers,httpheaders)
 end
 
 function godirectkey(d)
-	if d  == nil then return d end
-	local  _dkey = ""
+	if d == nil then return d end
+	local _dkey = ""
 	if d == 1 then
 		_dkey = RC.red
 	elseif d == 2 then
@@ -68,7 +94,6 @@ function godirectkey(d)
 	return _dkey
 end
 
-
 function hideMenu(menu)
 	if menu ~= nil then
 		menu:hide()
@@ -81,10 +106,8 @@ function epgInfo(xres, yres, aspectRatio, framerate)
 	local x = ((SCREEN['END_X'] - SCREEN['OFF_X']) - dx) / 2;
 	local y = ((SCREEN['END_Y'] - SCREEN['OFF_Y']) - dy) / 2;
 
-	local wh = cwindow.new{x=x, y=y, dx=dx, dy=dy, icon="", show_footer=false};
+	local wh = cwindow.new{x=x, y=y, dx=dx, dy=dy, title=Title, icon="", show_footer=false};
 	local ct = ctext.new{parent=wh, text=Epg, font_text=FONT['MENU'], mode="ALIGN_SCROLL | ALIGN_TOP"};
-
-	wh:setCaption{title=Title};
 	wh:paint()
 
 	repeat
@@ -103,28 +126,26 @@ end
 function play_live(_id)
 	local id = tonumber(_id)
 	if replayList[id].stream == nil then
-		getLiveStream(id)
+		getStream(id)
 	end
 	if replayList[id].stream then
 		hideMenu(replayMenu)
-		Epg = replayList[id].epg
-		Title = replayList[id].name
-		if Epg and Title then
+		Title = replayList[id].title
+		Epg = replayList[id].info1 .. "\n" .. replayList[id].info2
+		if Title and Epg ~= "\n" then
 			vPlay:setInfoFunc("epgInfo")
 		end
-		local info1 = "Replay"
-		if Title then
-			info1 = Title
-		end
-		vPlay:PlayFile(replayList[id].name, replayList[id].stream, info1, "", replayList[id].audiostream or "")
-		Epg = nil
+
+		vPlay:PlayFile(replayList[id].name, replayList[id].stream, replayList[id].title, replayList[id].info1, replayList[id].audiostream or "")
+
 		Title = nil
+		Epg = nil
 		repaint = true
 		return MENU_RETURN.EXIT
 	end
 end
 
-function getARDstream(id)
+function getStream_ARD(id)
 	local url = replayList[id].url
 	local jdata = getdata(url)
 	if jdata then
@@ -135,17 +156,17 @@ function getARDstream(id)
 	end
 end
 
-function getLiveStream(id)
+function getStream(id)
 	local url = replayList[id].url
 	if url and replayList[id].ch == "ard" then
-		getARDstream(id)
+		getStream_ARD(id)
 	end
 	return replayList[id].stream
 end
 
-function getReLiveList_ARD()
+function getReplayList_ARD()
 	local titleList = {}
-	local h = hintbox.new{caption="Please Wait ...", text="I'm Thinking."}
+	local h = hintbox.new{caption=locale[lang].wait, text=locale[lang].read_data}
 	if h then
 		h:paint()
 	end
@@ -187,7 +208,16 @@ function getReLiveList_ARD()
 				end
 				if duplicate == false then
 					table.insert(titleList, {title=jnTab.title})
-					table.insert(replayList, {name="ARD: " .. jnTab.name .. ' - ' .. jnTab.title, url=link, stream=nil, audiostream=nil, epg=detail, hint=subtitle, hasVideo=true, ch='ard'})
+					table.insert(replayList, {
+						name = "ARD: " .. jnTab.name,
+						title = jnTab.title,
+						url = link,
+						stream = nil,
+						audiostream = nil,
+						info1 = subtitle,
+						info2 = detail,
+						hasVideo = true,
+						ch = 'ard'})
 				end
 			end
 		end
@@ -198,34 +228,41 @@ function getReLiveList_ARD()
 	titleList = {}
 end
 
-function main_menu()
-	if repaint then
-		replayList = {}
-		getReLiveList_ARD()
-		repaint = false
-	end
+function mainMenu()
 	if #replayList == 0 then
 		return
 	end
-	replayMenu = menu.new{name="Replay", icon="streaming"}
+
+	replayMenu = menu.new{name=plugin_name, icon="streaming"}
 	replayMenu:addItem{type="separator"}
 	replayMenu:addItem{type="back"}
 	replayMenu:addItem{type="separatorline"}
-	local d =  0
+	local d = 0
 	for i, v in ipairs(replayList) do
 		d = d+1
-		replayMenu:addItem{type="forwarder", name=v.name, action="play_live", hint=v.hint, enabled=v.hasVideo, id=i, directkey=godirectkey(d)}
+		replayMenu:addItem{ type="forwarder",
+			action="play_live",
+			name=v.name .. " - " .. v.title,
+			hint=v.info1,
+			enabled=v.hasVideo,
+			id=i,
+			directkey=godirectkey(d)
+		}
 	end
 	replayMenu:exec()
 	replayMenu:hide()
 end
 
-function main()
-	init()
-	repeat
-		main_menu()
-		collectgarbage()
-	until repaint == false
-end
+-- ----------------------------------------------------------------------------
 
-main()
+getReplayList_ARD()
+
+repeat
+	mainMenu()
+	if repaint then
+		replayList = {}
+		getReplayList_ARD()
+		repaint = false
+	end
+	collectgarbage()
+until repaint == false
