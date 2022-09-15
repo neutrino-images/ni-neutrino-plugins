978912bbff973d571b4c7f5e3ab1be1e83796d84
vanhofen <vanhofen@gmx.de>
2022-09-15 21:49:51 +0200

    - add multibox

--- include/rc_device_hardcoded.h
+++ include/rc_device_hardcoded.h
@@ -16,6 +16,14 @@
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
 #endif
 
+#elif BOXMODEL_MULTIBOX
+#ifndef RC_DEVICE
+#define RC_DEVICE "/dev/input/event0"
+#endif
+#ifndef RC_DEVICE_FALLBACK
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+#endif
+
 #elif BOXMODEL_MULTIBOXSE
 #ifndef RC_DEVICE
 #define RC_DEVICE "/dev/input/event0"
