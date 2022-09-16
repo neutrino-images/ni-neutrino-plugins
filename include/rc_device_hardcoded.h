b959b540d61ffbab7ab1ed60534e05d9af6678f3
vanhofen <vanhofen@gmx.de>
2022-09-16 23:31:38 +0200

    - simplify RC_DEVICE defines

--- include/rc_device_hardcoded.h
+++ include/rc_device_hardcoded.h
@@ -16,31 +16,7 @@
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
 #endif
 
-#elif BOXMODEL_MULTIBOX
-#ifndef RC_DEVICE
-#define RC_DEVICE "/dev/input/event0"
-#endif
-#ifndef RC_DEVICE_FALLBACK
-#define RC_DEVICE_FALLBACK "/dev/input/event1"
-#endif
-
-#elif BOXMODEL_MULTIBOXSE
-#ifndef RC_DEVICE
-#define RC_DEVICE "/dev/input/event0"
-#endif
-#ifndef RC_DEVICE_FALLBACK
-#define RC_DEVICE_FALLBACK "/dev/input/event1"
-#endif
-
-#elif BOXMODEL_OSMIO4K
-#ifndef RC_DEVICE
-#define RC_DEVICE "/dev/input/event0"
-#endif
-#ifndef RC_DEVICE_FALLBACK
-#define RC_DEVICE_FALLBACK "/dev/input/event1"
-#endif
-
-#elif BOXMODEL_OSMIO4KPLUS
+#elif BOXMODEL_MULTIBOX || BOXMODEL_MULTIBOXSE || BOXMODEL_OSMIO4K || BOXMODEL_OSMIO4KPLUS
 #ifndef RC_DEVICE
 #define RC_DEVICE "/dev/input/event0"
 #endif
