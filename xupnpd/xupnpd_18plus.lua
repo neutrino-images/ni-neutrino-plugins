af18aa62bbf151e830b5044efd5062b5b58a167b
svenhoefer <svenhoefer@svenhoefer.com>
2023-03-07 09:07:05 +0100

    - xupnpd_18plus.lua: fixes by det-box and jokel
    Signed-off-by: GetAway <get-away@t-online.de>

--- xupnpd/xupnpd_18plus.lua
+++ xupnpd/xupnpd_18plus.lua
@@ -4,6 +4,8 @@
 -- ******************************
 -- 22.08.2021
 -- 27.06.2022 update by jokel
+-- 05.03.2023 update by det-box
+-- 06.03.2023 update by jokel
 
 cfg.user_age=18
 cfg.youporn_max_pages=5
@@ -13,27 +15,27 @@ cfg.maxRes=720
 
 youporn_category=
 {
-    ['top_rated']='/top_rated/', ['most_viewed']='/most_viewed/', ['amateur']='/category/1/amateur/', ['anal']='/category/2/anal/',
-    ['asian']='/category/3/asian/', ['bbw']='/category/4/bbw/', ['big_butt']='/category/6/big-butt/', ['big_tits']='/category/7/big-tits/',
-    ['bisexual']='/category/5/bisexual/', ['blonde']='/category/51/blonde/', ['blowjob']='/category/9/blowjob/',
-    ['brunette']='/category/52/brunette/', ['coed']='/category/10/coed/', ['compilation']='/category/11/compilation/',
-    ['couples']='/category/12/couples/', ['creampie']='/category/13/creampie/', ['cumshots']='/category/37/cumshots/',
-    ['cunnilingus']='/category/15/cunnilingus/', ['dp']='/category/16/dp/', ['ebony']='/category/8/ebony/',
-    ['european']='/category/48/european/', ['facial']='/category/17/facial/', ['fantasy']='/category/42/fantasy/',
-    ['fetish']='/category/18/fetish/', ['fingering']='/category/62/fingering/', ['funny']='/category/19/funny/',
-    ['gay']='/category/20/gay/', ['german']='/category/58/german/', ['gonzo']='/category/50/gonzo/',
-    ['group_sex']='/category/21/group-sex/', ['hairy']='/category/46/hairy/', ['handjob']='/category/22/handjob/',
-    ['hentai']='/category/23/hentai/', ['instructional']='/category/24/instructional/', ['interracial']='/category/25/interracial/',
-    ['interview']='/category/41/interview/', ['kissing']='/category/40/kissing/', ['latina']='/category/49/latina/',
-    ['lesbian']='/category/26/lesbian/', ['milf']='/category/29/milf/', ['masturbate']='/category/55/masturbate/',
-    ['mature']='/category/28/mature/', ['pov']='/category/36/pov/', ['panties']='/category/56/panties/',
-    ['pantyhose']='/category/57/pantyhose/', ['public']='/category/30/public/', ['redhead']='/category/53/redhead/',
-    ['rimming']='/category/43/rimming/', ['romantic']='/category/61/romantic/', ['shaved']='/category/54/shaved/',
-    ['shemale']='/category/31/trans/', ['solo_male']='/category/60/solo-male/', ['solo_girl']='/category/27/solo-girl/',
-    ['squirting']='/category/39/squirting/', ['strt_sex']='/category/47/strt-sex/', ['swallow']='/category/59/swallow/',
-    ['teen']='/category/32/teen/', ['threesome']='/category/38/threesome/', ['vintage']='/category/33/vintage/',
-    ['voyeur']='/category/34/voyeur/', ['webcam']='/category/35/webcam/', ['3d']='/category/63/3d/', ['hd']='/category/65/hd/',
-    ['young-old']='/category/45/young-old/'
+    ['top_rated']='/top_rated/', ['most_viewed']='/most_viewed/', ['amateur']='/category/amateur/', ['anal']='/category/anal/',
+    ['asian']='/category/asian/', ['bbw']='/category/bbw/', ['big_butt']='/category/big-butt/', ['big_tits']='/category/big-tits/',
+    ['bisexual']='/category/bisexual/', ['blonde']='/category/blonde/', ['blowjob']='/category/blowjob/',
+    ['brunette']='/category/brunette/', ['coed']='/category/coed/', ['compilation']='/category/compilation/',
+    ['couples']='/category/couples/', ['creampie']='/category/creampie/', ['cumshots']='/category/cumshots/',
+    ['cunnilingus']='/category/cunnilingus/', ['dp']='/category/dp/', ['ebony']='/category/ebony/',
+    ['european']='/category/european/', ['facial']='/category/facial/', ['fantasy']='/category/fantasy/',
+    ['fetish']='/category/fetish/', ['fingering']='/category/fingering/', ['funny']='/category/funny/',
+    ['gay']='/category/gay/', ['german']='/category/german/', ['gonzo']='/category/gonzo/',
+    ['group_sex']='/category/group-sex/', ['hairy']='/category/hairy/', ['handjob']='/category/handjob/',
+    ['hentai']='/category/hentai/', ['instructional']='/category/instructional/', ['interracial']='/category/interracial/',
+    ['interview']='/category/interview/', ['kissing']='/category/kissing/', ['latina']='/category/latina/',
+    ['lesbian']='/category/lesbian/', ['milf']='/category/milf/', ['masturbate']='/category/masturbate/',
+    ['mature']='/category/mature/', ['pov']='/category/pov/', ['panties']='/category/panties/',
+    ['pantyhose']='/category/pantyhose/', ['public']='/category/public/', ['redhead']='/category/redhead/',
+    ['rimming']='/category/rimming/', ['romantic']='/category/romantic/', ['shaved']='/category/shaved/',
+    ['shemale']='/category/trans/', ['solo_male']='/category/solo-male/', ['solo_girl']='/category/solo-girl/',
+    ['squirting']='/category/squirting/', ['strt_sex']='/category/strt-sex/', ['swallow']='/category/swallow/',
+    ['teen']='/category/teen/', ['threesome']='/category/threesome/', ['vintage']='/category/vintage/',
+    ['voyeur']='/category/voyeur/', ['webcam']='/category/webcam/', ['3d']='/category/3d/', ['hd']='/category/hd/',
+    ['young-old']='/category/young-old/'
 }
 
 function check_if_double(tab,name)
@@ -62,7 +64,7 @@ function youporn_updatefeed(feed,friendly_name)
 	if dfd then
 		dfd:write('#EXTM3U name=\"',friendly_name or feed_name,'\" type=mp4 plugin=youporn\n')
 -- 		http.user_agent(cfg.user_agent..'\r\nCookie: age_verified=1')
-		http.user_agent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) ' ..'\r\n')
+		http.user_agent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) ' ..'\r\nCookie: age_verified=1')
 		local page=1
 		local urls = {}
 		while(page<=cfg.youporn_max_pages) do
