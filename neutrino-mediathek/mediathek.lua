b01e283283195426931e558ecbba2a0f5b4698de
Thilo Graf <dbt@novatux.de>
2025-11-27 09:41:44 +0100

    refactor: move filter helpers into module

--- neutrino-mediathek/mediathek.lua
+++ neutrino-mediathek/mediathek.lua
@@ -31,7 +31,13 @@ local localRecordingsLastPath = nil
 local localRecordingsMode = false
 
 -- load shared helpers
+local Filters = dofile(pluginScriptPath .. '/filters.lua')
 dofile(pluginScriptPath .. '/util.lua')
+
+local sortEntries = Filters.sortEntries
+local requiresFullBuffer = Filters.requiresFullBuffer
+local matchesSearchFilters = Filters.matchesSearchFilters
+local entryMatchesFilters = Filters.entryMatchesFilters
 
 local function getCachePaths()
 	local configPath = (CONF_PATH or '/var/tuxbox/config/') .. H.scriptBase() .. '_local_recordings.json'
@@ -45,8 +51,6 @@ end
 function isLocalRecordingsMode()
 	return localRecordingsMode
 end
-
-local entryMatchesFilters
 
 local function joinPath(base, leaf)
 	if base == nil or base == '' then
@@ -1006,26 +1010,6 @@ function formatQualityMode()
 	return l.qualityFilterAll
 end
 
-entryMatchesFilters = function(entry)
-	if conf.hideAccessibilityHints == 'on' and isAccessibilityHintEntry(entry) then
-		return false
-	end
-	local geo = entry.geo or ''
-	if conf.geoMode == 'no_geo' and geo ~= '' then
-		return false
-	elseif conf.geoMode == 'only_geo' and geo == '' then
-		return false
-	end
-	local hasHD = entry.url_hd ~= nil and entry.url_hd ~= ''
-	local hasSD = entry.url ~= nil and entry.url ~= ''
-	if conf.qualityFilter == 'require_hd' and not hasHD then
-		return false
-	elseif conf.qualityFilter == 'require_sd' and not hasSD then
-		return false
-	end
-	return true
-end
-
 buildEntry = function(apiEntry)
 	return {
 		channel = apiEntry.channel,
@@ -1197,75 +1181,6 @@ function getLocalRecordingsStats()
 		end
 	end
 	return stats
-end
-
-sortEntries = function(list)
-	local mode = conf.sortMode
-	local function compare(a, b)
-		if mode == 'date_asc' then
-			return (a.timestamp or 0) < (b.timestamp or 0)
-		elseif mode == 'title_asc' then
-			return (a.title or ''):lower() < (b.title or ''):lower()
-		elseif mode == 'title_desc' then
-			return (a.title or ''):lower() > (b.title or ''):lower()
-		elseif mode == 'duration_desc' then
-			return (a.durationSec or 0) > (b.durationSec or 0)
-		elseif mode == 'duration_asc' then
-			return (a.durationSec or 0) < (b.durationSec or 0)
-		else -- date_desc default
-			return (a.timestamp or 0) > (b.timestamp or 0)
-		end
-	end
-	table.sort(list, compare)
-end
-
-requiresFullBuffer = function()
-	if conf.allThemes ~= 'on' or conf.allTitles ~= 'on' then
-		return true
-	end
-	if conf.sortMode ~= 'date_desc' then
-		return true
-	end
-	if conf.geoMode ~= 'all' or conf.qualityFilter ~= 'all' then
-		return true
-	end
-	return false
-end
-
-matchesSearchFilters = function(apiEntry)
-	if conf.allThemes ~= 'on' then
-		if (apiEntry.theme or '') ~= (conf.theme or '') then
-			return false
-		end
-	end
-
-	if conf.allTitles == 'on' then
-		return true
-	end
-
-	local needle = conf.title or ''
-	local hayTitle = apiEntry.title or ''
-	local hayDescr = apiEntry.description or ''
-
-	if conf.ignoreCase == 'on' then
-		needle = needle:upper()
-		hayTitle = hayTitle:upper()
-		hayDescr = hayDescr:upper()
-	end
-
-	if conf.partialTitle ~= 'on' then
-		return hayTitle == needle
-	end
-
-	if string.find(hayTitle, needle, 1, true) ~= nil then
-		return true
-	end
-
-	if conf.inDescriptionToo == 'on' and string.find(hayDescr, needle, 1, true) ~= nil then
-		return true
-	end
-
-	return false
 end
 
 function localRecordingsEntryEnabled()
